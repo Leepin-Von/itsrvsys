308716dc49366b15012acc85bdf774f32839532b
Leepin-Von <lipingvon1111@outlook.com>
2025-01-20 19:55:45 +0800

    传salt部分修改

--- src/main/java/com/plotech/kanban/service/impl/UserServiceImpl.java
+++ src/main/java/com/plotech/kanban/service/impl/UserServiceImpl.java
@@ -1,12 +1,12 @@
 package com.plotech.kanban.service.impl;
 
+import cn.hutool.crypto.digest.DigestUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.plotech.kanban.exception.CommonBaseException;
 import com.plotech.kanban.pojo.entity.User;
 import com.plotech.kanban.pojo.vo.TransferDataRequest;
 import com.plotech.kanban.pojo.vo.TransferDataResponse;
 import com.plotech.kanban.service.UserService;
-import com.plotech.kanban.util.SaltMD5Util;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -15,7 +15,9 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Random;
 
 @Slf4j
 @Service
@@ -39,12 +41,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean authConfirm(User user) {
-        // 不知道ERP里能用的账号密码，所以无奈写了这段测试用，用户名为test直接过
-        // 正式环境下要删掉
-//        log.info(JSONObject.toJSONString(user));
-//        if ("test".equalsIgnoreCase(user.getUsername())) return true;
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        String salt = sb.append(random.nextInt(99999999)).append(random.nextInt(99999999)).toString();
+        if (salt.length() < 16) {
+            for (int i = 0; i < 16 - salt.length(); i++) {
+                sb.append("0");
+            }
+        }
+        salt = sb.toString();
         // 对密码进行加盐MD5加密处理
-        user.setPassword(SaltMD5Util.generateSaltPassword(user.getPassword().toUpperCase()));
+        user.setPassword(Arrays.toString(DigestUtil.md5(user.getPassword().toUpperCase() + salt)));
         // 目标路径
         String targetUrl = getTargetUrl();
         // 请求体
@@ -54,6 +61,7 @@ public class UserServiceImpl implements UserService {
         parameters.put("UserId", user.getUsername());
         parameters.put("PWD", user.getPassword());
         parameters.put("ItemId", user.getItemId());
+        parameters.put("CheckNum", salt);
         request.setParameters(parameters);
         // 请求头
         HttpHeaders headers = new HttpHeaders();
